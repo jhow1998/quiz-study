1403cbe6fea986d356db626bd4ca480895371331
Joseph Robert <joseph.robert.carvalho@gmail.com>
2022-06-29 16:46:53 -0300

    falta conserta slide

---
+++ js/script.js
@@ -0,0 +1,47 @@
+let menuBtn = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .flex .navbar');
+
+menuBtn.onclick = () =>{
+    menuBtn.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+var swiper = new Swiper(".course-slider", {
+    spaceBetween: 20,
+    loop:true,
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    breakpoints: {
+        540: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+    },
+});
+
+var swiper = new Swiper(".teachers-slider", {
+  spaceBetween:20,
+  loop:true,
+  autoplay:{
+      delay:2500,
+      disableOnInteraction:false,
+  },
+  breakpoints:{
+      640:{
+          slidesPerView:1,
+      },
+      768:{
+          slidesPerView:2,
+      },
+      1024:{
+          slidesPerView:3,
+      },
+
+  },
+});
